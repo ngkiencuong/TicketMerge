9484d81fd2c28291926c50f91b0e12c7b4fd019d
Simplify-ERP® <github@euroblaze.de>
2023-04-16 00:15:46 +0200

    Create manifest.py

---
+++ manifest.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Helpdesk Ticket Merger',
+    'version': '1.0',
+    'category': 'Helpdesk',
+    'summary': 'Merge multiple helpdesk tickets',
+    'author': 'Your Name',
+    'website': 'https://simplify-erp.de',
+    'depends': ['helpdesk'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
