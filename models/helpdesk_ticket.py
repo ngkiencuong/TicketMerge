6ef39711ac6d98a0d45627f18315b356fbffbe00
ngkiencuong <85164180+ngkiencuong@users.noreply.github.com>
2023-04-25 16:55:23 +0700

    ...

--- models/helpdesk_ticket.py
+++ models/helpdesk_ticket.py
@@ -8,7 +8,10 @@ class HelpdeskTicket(models.Model):
     archived_tickets = fields.One2many(comodel_name="helpdesk.ticket", inverse_name="surviving_ticket", string="Archived Tickets", context={'active_test': False}, readonly=True)
 
     def _action_open_merge_tickets(self):
+        ctx = self._context.copy()
         active_tickets = self.env['helpdesk.ticket'].browse(self._context.get('active_ids', []))
+        ctx['partner_ids'] = active_tickets.mapped('partner_id.id')
+        ctx['user_ids'] = active_tickets.mapped('user_id.id')
         return {
             'name': 'Merge Tickets',
             'view_mode': 'form',
@@ -16,8 +19,5 @@ class HelpdeskTicket(models.Model):
             'type': 'ir.actions.act_window',
             'view_id': self.env.ref('TicketMerge.merge_ticket_view_form').id,
             'target': 'new',
-            'context': {
-                'partner_ids': active_tickets.mapped('partner_id.id'),
-                'user_ids': active_tickets.mapped('user_id.id'),
-            }
+            'context': ctx,
         }
--- wizard/merge_ticket.py
+++ wizard/merge_ticket.py
@@ -17,30 +17,44 @@ class MergeTickets(models.TransientModel):
     partner_id = fields.Many2one('res.partner', string='Customer', domain=_domain_partner_id, help='You can only merge tickets from the same customer')
     user_id = fields.Many2one('res.users', string='Assigned to', domain=_domain_user_id, help='This will be the assignee for the surviving ticket')
 
-    def merge_ticket(self):
-        # Get the tickets to merge
+    def action_confirm(self):
         tickets = self.env['helpdesk.ticket'].browse(self._context.get('active_ids', []))
+        old_tickets = tickets.filtered(lambda x: x.archived_tickets)
+        tickets = tickets + tickets.mapped('archived_tickets') - old_tickets
+        new_ticket = self.merge_ticket(tickets)
+        for rec in old_tickets:
+            self.merge_chatter_attachment(rec, new_ticket)
+            rec.timesheet_ids.sudo().write({'helpdesk_ticket_id': new_ticket.id})
+        # old_tickets.write({'active': False})
+
+    def merge_ticket(self, tickets):
         # Find the oldest ticket
         surviving_ticket = min(tickets, key=lambda t: t.create_date)
+        new_ticket = surviving_ticket.copy()
+        for mess in self.env['mail.message'].search([('res_id', '=', new_ticket.id), ('model', '=', 'helpdesk.ticket')]):
+            mess.sudo().unlink()
         # Merge titles, descriptions, assignee and customer
-        surviving_ticket = surviving_ticket.sudo()
-        surviving_ticket.name = ' '.join(tickets.mapped('name'))
-        surviving_ticket.description = ' '.join(tickets.filtered_domain([('description', '!=', False)]).mapped('description'))
-        surviving_ticket.user_id = self.user_id
-        surviving_ticket.partner_id = self.partner_id
+        new_ticket.name = ' '.join(tickets.mapped('name'))
+        new_ticket.description = ' '.join(tickets.filtered_domain([('description', '!=', False)]).mapped('description'))
+        new_ticket.user_id = self.user_id
+        new_ticket.partner_id = self.partner_id
         # Merge timesheets
-        for ticket in tickets - surviving_ticket:
-            if ticket.timesheet_ids and not surviving_ticket.team_id.use_helpdesk_timesheet:
+        for ticket in tickets:
+            if ticket.timesheet_ids and not new_ticket.team_id.use_helpdesk_timesheet:
                 raise ValidationError(_("Team in the surviving ticket doesn't allow timesheet. Please check it!"))
             ticket = ticket.sudo()
-            ticket.timesheet_ids.write({'helpdesk_ticket_id': surviving_ticket.id})
+            ticket.timesheet_ids.write({'helpdesk_ticket_id': new_ticket.id})
             # Archive the ticket
             ticket.active = False
             # Add the link to the archived ticket
-            ticket.surviving_ticket = surviving_ticket.id
+            ticket.surviving_ticket = new_ticket.id
             # merge chatter note and attachment
-            for mess in self.env['mail.message'].search([('res_id', '=', ticket.id), ('model', '=', 'helpdesk.ticket')], order='id'):
-                mess.sudo().write({'res_id': surviving_ticket.id})
-            for attach in self.env['ir.attachment'].search([('res_id', '=', ticket.id), ('res_model', '=', 'helpdesk.ticket')]):
-                attach.sudo().write({'res_id': surviving_ticket.id})
-        return surviving_ticket
+            self.merge_chatter_attachment(ticket, new_ticket)
+        return new_ticket
+
+    def merge_chatter_attachment(self, old_ticket, new_ticket):
+        for mess in self.env['mail.message'].search([('res_id', '=', old_ticket.id), ('model', '=', 'helpdesk.ticket')]):
+            mess.sudo().write({'res_id': new_ticket.id})
+        for attach in self.env['ir.attachment'].search([('res_id', '=', old_ticket.id), ('res_model', '=', 'helpdesk.ticket')]):
+            attach.sudo().write({'res_id': new_ticket.id})
+
